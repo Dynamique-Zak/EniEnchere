8e03936b7104c009604eef2d09282ece8ee79b12
Schartier Isaac <dynamique.zak@gmail.com>
2022-02-15 10:27:45 +0100

    Test main

---
+++ EniEnchereCommun/src/fr/eni/enchere/main/Main.java
@@ -0,0 +1,10 @@
+package fr.eni.enchere.main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test projet");
+	}
+
+}
